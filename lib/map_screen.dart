25db48d7786b7f0cee59bb860d84c6924bcd8be2
Igor <dsw51706@student.dsw.edu.pl>
2026-01-04 13:26:18 +0100

    mozliwosc powrotu z ekranu mapy

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -5,8 +5,11 @@ class MapScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SizedBox.expand(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mapa'),
+      ),
+      body: const SizedBox.expand(),
     );
   }
 }
